594d5efb7c39d02e8393db37462056bb81b776d4
Juraj TOMKA <porterbox+github@gmail.com>
2018-04-05 10:47:59 +0200

    Add Wunderground data
    Weather data for region Nitra

---
+++ esp/wunderground.php
@@ -0,0 +1,25 @@
+<?php
+	$url = 'http://api.wunderground.com/api/f95a6e63f61d64c3/conditions/q/SK/Nitra.json';
+
+	$json = file_get_contents($url);
+	$root = json_decode($json);
+	$data = $root->current_observation;
+
+	$content = 
+		'var wunderground = {'.
+			'"date":"'. date('j.n G:i', $data->observation_epoch) .'",'.
+			'"location":"'. $data->display_location->full .'",'.
+			'"temperature":"'. $data->temp_c .'",'.
+			'"feelslike":"'. $data->feelslike_c .'",'.
+			'"humidity":"'. $data->relative_humidity .'",'.
+			'"dewpoint":"'. $data->dewpoint_c .'",'.
+			'"pressure":"'. $data->pressure_mb .'"'.
+		'};';
+
+	/*
+	echo '<pre>';
+	print_r($root->current_observation);
+	echo '</pre>';
+	*/
+
+	echo $content;
