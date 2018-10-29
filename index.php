b7a5981461dc250bfe3329bafe690f645c17868d
Juraj TOMKA <porterbox+github@gmail.com>
2018-10-29 23:35:37 +0100

    dst

--- index.php
+++ index.php
@@ -115,11 +115,12 @@
 					$('.fancontrol-brhumidity').html(Math.round(fancontroldata.brHumidity) + '%' + shower);
 					
 					var brUpdateOn = new Date(fancontroldata.brUpdateOn * 1000);
-					
+					/*
 					if (isDST(brUpdateOn))
 					{
 						brUpdateOn = new Date((fancontroldata.brUpdateOn - 3600) * 1000);
 					}
+					*/
 					
 					var brHours = ("0" + brUpdateOn.getUTCHours()).slice(-2);
 					var brMinutes = ("0" + brUpdateOn.getUTCMinutes()).slice(-2);
