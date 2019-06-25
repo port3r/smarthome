b1d4a1daf3a065714b4c66b8615f3d482496ab7b
Juraj TOMKA <porterbox+github@gmail.com>
2019-06-25 10:38:46 +0200

    create cam.php

---
+++ esp/cam.php
@@ -0,0 +1,18 @@
+<?php
+	$localUrl = 'http://192.168.0.73';
+	if (!empty($_GET['redirect']))
+	{
+		// -- PING
+		if ($_GET['redirect'] == 'index')
+		{
+			$_GET['redirect'] = '';
+		}
+		
+		$content = implode('', file($localUrl .'/'. $_GET['redirect']));
+		echo $content;
+	}
+	else
+	{
+		$content = implode('', file($localUrl .'/getdata.js'));
+		echo $content;
+	}	
