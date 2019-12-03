dfe02b6f58960c0215faaae94ab49c029bdb26f5
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-03 20:55:51 +0100

    Update video.php

--- esp/video.php
+++ esp/video.php
@@ -1,2 +1,3 @@
-header('Content-Type: image/jpeg');
-readfile('http://192.168.0.73/cgi/video/video.cgi');
+<?php
+  header('Content-Type: image/jpeg');
+  readfile('http://192.168.0.73/cgi/video/video.cgi');
