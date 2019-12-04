74289487ef00418976d2f05d6a4a2e0f4acf3735
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-04 13:31:25 +0100

    curl

--- esp/video.php
+++ esp/video.php
@@ -1,3 +1,14 @@
 <?php
-  header('Content-Type: image/jpeg');
-  readfile('http://192.168.0.73/cgi/jpg/image.cgi');
+  //header('Content-Type: image/jpeg');
+  //readfile('http://192.168.0.73/cgi/jpg/image.cgi');
+  
+  header('content-type: multipart/x-mixed-replace; boundary=--myboundary');
+  
+  while (@ob_end_clean()); 
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'http://192.168.0.73/axis-cgi/mjpg/video.cgi?resolution=320x240'); 
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  $im = curl_exec($ch);
+
+  echo $im;
+  curl_close($ch);
