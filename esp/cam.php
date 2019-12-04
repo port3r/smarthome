13b32fc3e9530db09f75e874d8ed58f57a4c44ec
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-04 14:45:09 +0100

    Update cam.php

--- esp/cam.php
+++ esp/cam.php
@@ -21,5 +21,5 @@
 	{
 		header('Content-Type: multipart/x-mixed-replace; boundary=myboundary');
     		ob_end_flush();
-    		readfile('http://'. $localUrl .'/video.cgi?resolution=VGA');
+    		readfile('http://'. $localUrl .'/-wvhttp-01-/video.cgi');
 	}	
