d9140d2c307c90c4be6e64450c6858afe1ef5c19
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-04 13:46:49 +0100

    Update video.php

--- esp/video.php
+++ esp/video.php
@@ -6,7 +6,7 @@
   
   while (@ob_end_clean()); 
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'http://192.168.0.73/axis-cgi/mjpg/video.cgi?resolution=320x240'); 
+  curl_setopt($ch, CURLOPT_URL, 'http://192.168.0.73/cgi/video/video.cgi'); 
   curl_setopt($ch, CURLOPT_HEADER, 0);
   $im = curl_exec($ch);
 
