c502fe793fa53851743de7de576f2973529f6c10
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-04 14:19:00 +0100

    Update video.php

--- esp/video.php
+++ esp/video.php
@@ -2,16 +2,20 @@
   //header('Content-Type: image/jpeg');
   //readfile('http://192.168.0.73/cgi/jpg/image.cgi');
 
-# To stop apache from killing the script
-set_time_limit(0);
+header('Content-type: multipart/x-mixed-replace; boundary=focus73');
+$useragent="Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
 
-# Sending the correct header
-# The boundary=ipcamera is important.
-# You will have to change "ipcamera" to whatever your camera uses to seperate the 
-# images. Mine uses "--ipcamera" as seperator. You can omit the leading --, but have
-# to use the rest.
-header('Content-Type: multipart/x-mixed-replace;boundary=ipcamera');
+while (@ob_end_clean());
+//	header('Content-type: image/jpeg');
+// create curl resource
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+curl_setopt($ch, CURLOPT_URL, 'http://192.168.0.73/cgi/video/video.cgi');
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
-# Sending the images
-# You probably have to adapt the url to the video screen of your cam.
-readfile('http://192.168.0.73/cgi/video/video.cgi');
+// $output contains the output string
+$output = curl_exec($ch);
+echo $output;
+
+// close curl resource to free up system resources
+curl_close($ch);
