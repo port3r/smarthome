9fc5037b79e6e9e35ed4c2a7e3d195ba217ff9c7
Juraj TOMKA <porterbox+github@gmail.com>
2019-12-04 14:27:00 +0100

    Update video.php

--- esp/video.php
+++ esp/video.php
@@ -1,7 +1,8 @@
 <?php
-  //header('Content-Type: image/jpeg');
-  //readfile('http://192.168.0.73/cgi/jpg/image.cgi');
+  header('Content-Type: image/jpeg');
+  readfile('http://192.168.0.73/cgi/jpg/image.cgi');
 
+/*
 header('Content-type: multipart/x-mixed-replace; boundary=focus73');
 $useragent="Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
 
@@ -19,3 +20,4 @@ echo $output;
 
 // close curl resource to free up system resources
 curl_close($ch);
+*/
